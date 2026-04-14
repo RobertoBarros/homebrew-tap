8bef89b0d5f253aba8b4b22c6b92b796d465448c
Roberto Barros <roberto.f.barros@gmail.com>
2026-04-14 16:02:00 -0300

    lazyrspec v0.1.19

--- Formula/lazyrspec.rb
+++ Formula/lazyrspec.rb
@@ -1,10 +1,18 @@
 class Lazyrspec < Formula
   desc "A lazy TUI for running RSpec tests"
   homepage "https://github.com/RobertoBarros/lazyrspec"
-  url "https://github.com/RobertoBarros/lazyrspec/releases/download/v0.1.9/lazyrspec-0.1.9-darwin-arm64.tar.gz"
-  sha256 "12ebceb49280c978d3b13b3cd1a74fb54fcc7bf6f0cc2af622c5503c947f0858"
-  version "0.1.9"
+  version "0.1.19"
   license "MIT"
+
+  on_arm do
+    url "https://github.com/RobertoBarros/lazyrspec/releases/download/v0.1.19/lazyrspec-0.1.19-darwin-arm64.tar.gz"
+    sha256 "57bc55ff13a2b3b0f01e0c0874074329b258042929fdb8e364da59ed09f963e2"
+  end
+
+  on_intel do
+    url "https://github.com/RobertoBarros/lazyrspec/releases/download/v0.1.19/lazyrspec-0.1.19-darwin-x86_64.tar.gz"
+    sha256 "7426ccb5b163239012e5c82469a336efaa7678c6073a129779f10bad4f8a00a1"
+  end
 
   def install
     bin.install "lazyrspec"
