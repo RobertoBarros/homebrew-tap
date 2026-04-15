34fa21bc6b9cb9b72d358f58148000a258168eec
Roberto Barros <roberto.f.barros@gmail.com>
2026-04-15 15:31:15 -0300

    lazyrspec v0.1.27

--- Formula/lazyrspec.rb
+++ Formula/lazyrspec.rb
@@ -1,32 +1,32 @@
 class Lazyrspec < Formula
   desc "A lazy TUI for running RSpec tests"
   homepage "https://github.com/RobertoBarros/lazyrspec"
-  version "0.1.22"
+  version "0.1.26"
   license "MIT"
 
   bottle :unneeded
 
   on_macos do
     on_arm do
-      url "https://github.com/RobertoBarros/lazyrspec/releases/download/v0.1.22/lazyrspec-0.1.22-darwin-arm64.tar.gz"
-      sha256 "58923a79003b1c83ed9bcb7f08b5c438ee0b4f0c878e63db1d530e4fa2cb57ff"
+      url "https://github.com/RobertoBarros/lazyrspec/releases/download/v0.1.26/lazyrspec-0.1.26-darwin-arm64.tar.gz"
+      sha256 "6f1cbe3376590053e47c2d793450428f860c323d318607c84674e61e0ac30624"
     end
 
     on_intel do
-      url "https://github.com/RobertoBarros/lazyrspec/releases/download/v0.1.22/lazyrspec-0.1.22-darwin-x86_64.tar.gz"
-      sha256 "6bbe9249bd1d75a0a6d7a146dff448a2e0c3ecbccf4c77a1a9c3a673b974fe38"
+      url "https://github.com/RobertoBarros/lazyrspec/releases/download/v0.1.26/lazyrspec-0.1.26-darwin-x86_64.tar.gz"
+      sha256 "67b6b8b6f66820bb767d7ae99c47a141da1802ea5b33ae5d4dc7f6f946afeee4"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/RobertoBarros/lazyrspec/releases/download/v0.1.22/lazyrspec-0.1.22-linux-arm64.tar.gz"
-      sha256 "a11b847a8046bf04f3a367fe73c803562fece844e1f1336f9a364b923dc5ef4b"
+      url "https://github.com/RobertoBarros/lazyrspec/releases/download/v0.1.26/lazyrspec-0.1.26-linux-arm64.tar.gz"
+      sha256 "b63bdc22e3ff3bc36430382d847b16dde045d3592a3d7f7a1c2ea47c69f72073"
     end
 
     on_intel do
-      url "https://github.com/RobertoBarros/lazyrspec/releases/download/v0.1.22/lazyrspec-0.1.22-linux-x86_64.tar.gz"
-      sha256 "d324abcda970864df148fb23ad1164c2f2fa55cf552320ddf55c3dc3cea6f029"
+      url "https://github.com/RobertoBarros/lazyrspec/releases/download/v0.1.26/lazyrspec-0.1.26-linux-x86_64.tar.gz"
+      sha256 "1d162d05e545f9d72e4c94be04010bed8afe923629f276582e65fdca86ae4e6e"
     end
   end
 
